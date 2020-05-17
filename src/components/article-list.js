6c056069a678ec3854c519f9e8f3239bd73af6eb
Gihwan Kim <ghkim3221@gmail.com>
2020-05-18 00:12:43 +0900

    Move ArticleLink component inside article-list.js as ArticleListItem

--- src/components/article-list.js
+++ src/components/article-list.js
@@ -1,12 +1,35 @@
 import React from "react"
 import PropTypes from "prop-types"
+import styled from "styled-components"
+import { Link } from "gatsby"
 
-import ArticleLink from "./article-link"
+const StyledArticleLink = styled(props => <Link {...props} />)`
+  color: #000000;
+  text-decoration: none;
+  border-bottom: 1px solid #ffd55e;
+`
+
+const StyledArticleDate = styled.span`
+  margin-right: 1rem;
+  font-size: 1rem;
+`
+
+const ArticleListItem = ({ article }) => (
+  <div>
+    <h2>
+      <StyledArticleDate>{article.frontmatter.date}</StyledArticleDate>
+      <StyledArticleLink to={article.frontmatter.slug}>
+        {article.frontmatter.title}
+      </StyledArticleLink>
+    </h2>
+    <p>{article.excerpt}</p>
+  </div>
+)
 
 const ArticleList = ({ articles }) => (
   <div>
     {articles.map(article => (
-      <ArticleLink key={article.id} article={article} />
+      <ArticleListItem key={article.id} article={article} />
     ))}
   </div>
 )
