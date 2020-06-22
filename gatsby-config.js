73cd5a7c997cdd7f24befb1928fccd064b9af827
Gihwan Kim <ghkim3221@gmail.com>
2020-06-22 23:22:22 +0900

    Remove gatsby-transformer-remark dependency

--- gatsby-config.js
+++ gatsby-config.js
@@ -8,9 +8,6 @@ module.exports = {
       }
     },
     {
-      resolve: `gatsby-transformer-remark`
-    },
-    {
       resolve: `gatsby-plugin-google-analytics`,
       options: {
         trackingId: "UA-138964328-2"
